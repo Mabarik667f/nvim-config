f042722a521996b95b87d33e6db190bb77a3b0e1
mamba <jc.bob1412@gmail.com>
2025-11-04 15:46:06 +0300

    golangci-lint: lsp and formatters and linters integration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,79 +1,106 @@
-local okCfg, lspconfig = pcall(require, "lspconfig")
-if okCfg then
-	local okStatus, lspstatus = pcall(require, "lsp-status")
-	if okStatus then
-		lspstatus.config({
-			status_symbol = "LSP",
-			indicator_errors = "E",
-			indicator_warnings = "W",
-			indicator_info = "I",
-			indicator_hint = "H",
-		})
-		local servers = {
-			"ruff",
-			"rust_analyzer",
-			"gopls",
-			"dockerls",
-			"docker_compose_language_service",
-			"vls",
-			"clangd",
-			"tailwindcss",
-			"ts_ls",
-			"nil_ls",
-		}
+local okcfg, lspconfig = pcall(require, "lspconfig")
 
-		local capabilities =
-			vim.tbl_extend("keep", lspstatus.capabilities, require("cmp_nvim_lsp").default_capabilities())
+local configs = require 'lspconfig/configs'
+if not configs.golangcilsp then
+  configs.golangcilsp = {
+    default_config = {
+      cmd = { 'golangci-lint-langserver' },
+      root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
+      init_options = {
+        command = { "golangci-lint", "run", "--output.json.path", "stdout", "--show-stats=false", "--issues-exit-code=1" },
+      },
+    }
+  }
+end
 
-		lspconfig.pyright.setup({
-			capabilities = capabilities,
-			settings = {
-				pyright = {
-					disableOrganizeImports = true,
-				},
-				python = {
-					analysis = {
-						ignore = { "*" },
-					},
-				},
-			},
-		})
+if okcfg then
+  local okstatus, lspstatus = pcall(require, "lsp-status")
+  if okstatus then
+    lspstatus.config({
+      status_symbol = "lsp",
+      indicator_errors = "e",
+      indicator_warnings = "w",
+      indicator_info = "i",
+      indicator_hint = "h",
+    })
+    local servers = {
+      "ruff",
+      "rust_analyzer",
+      "gopls",
+      "dockerls",
+      "docker_compose_language_service",
+      "vls",
+      "clangd",
+      "tailwindcss",
+      "ts_ls",
+    }
 
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-				},
-			},
-		})
-		for _, lsp in ipairs(servers) do
-			lspconfig[lsp].setup({
-				capabilities = capabilities,
-			})
-		end
+    local capabilities =
+        vim.tbl_extend("keep", lspstatus.capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-	--	local ok, tsApi = pcall(require, "typescript-tools.api")
-	--	if ok then
-	--		local okTools, tsTools = pcall(require, "typescript-tools")
-	--		if okTools then
-	--			tsTools.setup({
-	--				capabilities = capabilities,
-	--				handlers = {
-	--					["textDocument/publishDiagnostics"] = tsApi.filter_diagnostics({ 80001 }),
-	--				},
-	--			})
-	--		else
-	--			vim.notify("typescript-tools failed to load:" .. tsTools, vim.log.levels.ERROR)
-	--		end
-	--	else
-	--		vim.notify("typescript-tools.api failed to load:" .. tsApi, vim.log.levels.ERROR)
-	--	end
-	else
-		vim.notify("lspstatus failed to load:" .. lspstatus, vim.log.levels.ERROR)
-	end
+    lspconfig.golangci_lint_ls.setup {
+      filetypes = { 'go', 'gomod' }
+    }
+
+    lspconfig.nil_ls.setup({
+      settings = {
+        ['nil'] = {
+          formatting = {
+            command = { "nixfmt" }
+          }
+        }
+      }
+    })
+
+    lspconfig.pyright.setup({
+      capabilities = capabilities,
+      settings = {
+        pyright = {
+          disableOrganizeImports = true,
+        },
+        python = {
+          analysis = {
+            ignore = { "*" },
+          },
+        },
+      },
+    })
+
+    lspconfig.lua_ls.setup({
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      },
+    })
+    for _, lsp in ipairs(servers) do
+      lspconfig[lsp].setup({
+        capabilities = capabilities,
+      })
+    end
+
+    --	local ok, tsApi = pcall(require, "typescript-tools.api")
+    --	if ok then
+    --		local okTools, tsTools = pcall(require, "typescript-tools")
+    --		if okTools then
+    --			tsTools.setup({
+    --				capabilities = capabilities,
+    --				handlers = {
+    --					["textDocument/publishDiagnostics"] = tsApi.filter_diagnostics({ 80001 }),
+    --				},
+    --			})
+    --		else
+    --			vim.notify("typescript-tools failed to load:" .. tsTools, vim.log.levels.ERROR)
+    --		end
+    --	else
+    --		vim.notify("typescript-tools.api failed to load:" .. tsApi, vim.log.levels.ERROR)
+    --	end
+  else
+    vim.notify("lspstatus failed to load:" .. lspstatus, vim.log.levels.ERROR)
+  end
 else
-	vim.notify("lspconfig failed to load:" .. lspconfig, vim.log.levels.ERROR)
+  vim.notify("lspconfig failed to load:" .. lspconfig, vim.log.levels.ERROR)
 end
--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -1,32 +1,61 @@
 local ok, none_ls = pcall(require, "null-ls")
 if not ok then
-	vim.notify("null-ls failed to load: " .. none_ls, vim.log.levels.ERROR)
-	return
+  vim.notify("null-ls failed to load: " .. none_ls, vim.log.levels.ERROR)
+  return
 end
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 none_ls.setup({
-	debounce = 500,
-	sources = {
-		none_ls.builtins.diagnostics.golangci_lint.with({
-			condition = function()
-				return vim.fn.executable("golangci-lint") == 1
-			end,
-		}),
-		none_ls.builtins.diagnostics.selene.with({
-			condition = function()
-				return vim.fn.executable("selene") == 1
-			end,
-		}),
-		none_ls.builtins.formatting.stylua.with({
-			condition = function()
-				return vim.fn.executable("stylua") == 1
-			end,
-		}),
-		none_ls.builtins.formatting.prettier.with({
-			condition = function()
-				return vim.fn.executable("prettier") == 1
-			end,
-		}),
-		none_ls.builtins.code_actions.gitsigns,
-	},
+  debounce = 500,
+  sources = {
+    none_ls.builtins.diagnostics.golangci_lint.with({
+      condition = function()
+        return vim.fn.executable("golangci-lint") == 1
+      end,
+    }),
+
+    none_ls.builtins.formatting.gofmt,
+    none_ls.builtins.formatting.gofumpt,
+    none_ls.builtins.formatting.goimports_reviser,
+    none_ls.builtins.formatting.goci,
+    none_ls.builtins.formatting.golines,
+
+    none_ls.builtins.diagnostics.selene.with({
+      condition = function()
+        return vim.fn.executable("selene") == 1
+      end,
+    }),
+    none_ls.builtins.formatting.stylua.with({
+      condition = function()
+        return vim.fn.executable("stylua") == 1
+      end,
+    }),
+    none_ls.builtins.formatting.prettier.with({
+      condition = function()
+        return vim.fn.executable("prettier") == 1
+      end,
+    }),
+    none_ls.builtins.formatting.nixpkgs_fmt.with({
+      condition = function()
+        return vim.fn.executable("nixpkgs_fmt") == 1
+      end,
+    }),
+    none_ls.builtins.code_actions.gitsigns,
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 })
