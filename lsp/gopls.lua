cd56fb0817f58e7573e1d04c86fa6cec72e92503
Mabarik667f <jc.bob1412@gmail.com>
2025-12-29 16:36:05 +0300

    feat: add tag for gopls

--- lsp/gopls.lua
+++ lsp/gopls.lua
@@ -2,4 +2,9 @@ return {
   cmd = { "gopls" },
   filetypes = { "go", "gomod", "gowork", "gotmpl", "gosum" },
   root_markers = { "go.mod", "go.work" },
+  settings = {
+    gopls = {
+      buildFlags = { "-tags=integration" },
+    },
+  },
 }
